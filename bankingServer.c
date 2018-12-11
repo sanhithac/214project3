ac3555576b0048c734416a19e6cafd2b9f2b797c
sanhithac <sanhitha.c@gmail.com>
2018-12-10 22:50:41 -0500

    Update bankingServer.c

--- bankingServer.c
+++ bankingServer.c
@@ -7,6 +7,8 @@ typedef struct account{
 };
 
 
+
+
 boolean checkBankName(struct accounts** head_ref, char *name){ //check if the bankAccount already exists
 	account *ptr = head_ref;
 	while(ptr != NULL){
@@ -29,17 +31,19 @@ int create(account* acc, char* name){
 	return 0;
 }
 
-int serve(accounts ** head_ref, char* name, pthread_mutex_t *_____){
+int serve(accounts ** head_ref, char* name, pthread_mutex_t *mut){
 	account *ptr = head_ref;
 	while(ptr != NULL){
 		if(ptr->accountName == name){
 			ptr->inSession=true;
+			pthread_mutex_lock(&mut);
 			printf("Session service started!");
 			return 0;
 		}
 		ptr = ptr->next;
 	}
 	printf("Error: Account not found");
+	return 0;
 }
 
 int deposit(account* acc, double amount){
@@ -72,9 +76,11 @@ int withdraw(account* acc, double amount){
 	return 0;
 }
 
-int endInSession(accounts *acc, pthread_mutex_t *_______){
+int end(accounts *acc, pthread_mutex_t *mut){
 	acc->inSession=false;
+	pthread_mutex_unlock(&mut);
 	printf("Session ended!");
+	return 0;
 }
 
 void printAccounts(accounts **head_ref){ //print account every 15 secs
